0093c425cd98862c3a5f91af049db732714bb928
sasholilov <alex_xld@abv.bg>
2023-01-09 00:00:00 +0200

    Remove account

--- script.js
+++ script.js
@@ -158,6 +158,22 @@ btnTransfer.addEventListener('click', function (e) {
     updateUI(currentAccount);
   }
 });
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    accounts.splice(index, 1);
+    containerApp.style.opacity = 0;
+  }
+  inputCloseUsername.value = inputClosePin.value = '';
+});
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -191,6 +207,7 @@ const deposits = movements.filter(function (mov) {
 const withdrawals = movements.filter(function (mov) {
   return mov < 0;
 });
+
 console.log(deposits);
 console.log(withdrawals);
 ////////Coding Challenge #1///////////////
